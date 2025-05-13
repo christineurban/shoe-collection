326991006fdef9c3ed477280dac7639326d9eed5
Christine Urban <christine.urban@pepsico.com>
2025-05-13 13:37:10 -0700

    Update rules, remove unused image capture component

--- src/components/ImageCapture/index.styled.ts
+++
@@ -1,8 +0,0 @@
-export {
-  StyledImageSection,
-  StyledImagePreview,
-  StyledImagePlaceholder,
-  StyledButtonGroup,
-  StyledImageCaptureButton,
-  StyledImageInput
-} from '../AddEditForm/index.styled';
